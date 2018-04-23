aa4b90aeb22038f1422b18f3c1bdb7c2b5eeb5f4
zhoujun <572459439@qq.com>
2018-04-23 14:59:01 +0800

    预测脚本支持提概率

--- predict.py
+++ predict.py
@@ -1,12 +1,11 @@
 import torch
-import matplotlib.pyplot as plt
-from torch import nn
+import torch.nn.functional as F
 from torch.autograd import Variable
 from torchvision import transforms
 import time
 import cv2
 import numpy as np
-from PIL import Image
+
 
 def softmax(x):
     x = x - np.max(x)
@@ -14,23 +13,23 @@ def softmax(x):
     softmax_x = exp_x / np.sum(exp_x)
     return softmax_x
 
+
 class Pytorch_model:
-    def __init__(self,model_path,img_shape, gpu_id = None,classes = None):
+    def __init__(self, model_path, img_shape, gpu_id=None, classes=None):
         self.gpu_id = gpu_id
         self.img_shape = img_shape
-        if self.gpu_id is not None and isinstance(self.gpu_id,int):
+        if self.gpu_id is not None and isinstance(self.gpu_id, int):
             self.use_gpu = True
         else:
             self.use_gpu = False
 
-
         if not self.use_gpu:
-            self.net = torch.load(model_path,map_location=lambda storage, loc: storage.cpu())
+            self.net = torch.load(model_path, map_location=lambda storage, loc: storage.cpu())
         else:
             self.net = torch.load(model_path, map_location=lambda storage, loc: storage.cuda(gpu_id))
         self.net.eval()
 
-    def predict(self,image_path):
+    def predict(self, image_path,topk=1):
         img = cv2.imread(image_path)
         img = cv2.resize(img, (self.img_shape[0], self.img_shape[1]))
 
@@ -43,36 +42,36 @@ class Pytorch_model:
         else:
             tensor = tensor.cpu()
 
-        outputs = self.net(tensor)
-        _, prediction_tensor = torch.max(outputs.data, 1)
-
+        outputs = F.softmax(self.net(tensor))
+        result = torch.topk(outputs.data[0],k=topk)
         if self.use_gpu:
-            prediction = prediction_tensor.cpu().numpy()[0]
+            result = result[1].cpu().numpy().tolist(), result[0].cpu().numpy().tolist()
         else:
-            prediction = prediction_tensor.numpy()[0]
-        return prediction
+            result = result[1].numpy().tolist(), result[0].numpy().tolist()
+
+        return result
 
 
 if __name__ == '__main__':
     img_path = r'/data/datasets/mnist/mnist_img/test/4/1.jpg'
-    model_path = 'resnet18.pkl'
+    model_path = 'resnet50.pkl'
 
-    model = Pytorch_model(model_path,img_shape=[224,224])
+    model = Pytorch_model(model_path, img_shape=[224, 224])
     start_cpu = time.time()
-    epoch = 1
+    epoch = 100
     for _ in range(epoch):
         start = time.time()
         result = model.predict(img_path)
-        print('device: cpu, result:%d, time: %.4f' % ( result,time.time()-start) )
+        print('device: cpu, result:%s, time: %.4f' % (str(result), time.time() - start))
     end_cpu = time.time()
 
     # test gpu speed
-    # model1 = Pytorch_model(model_path=model_path,img_shape=[224,224], gpu_id=7)
-    # start_gpu = time.time()
-    # for _ in range(epoch):
-    #     start = time.time()
-    #     result = model1.predict(img_path)
-    #     print('device: gpu, result:%d, time: %.4f' % ( result,time.time()-start) )
-    # end_gpu = time.time()
+    model1 = Pytorch_model(model_path=model_path,img_shape=[224,224], gpu_id=0)
+    start_gpu = time.time()
+    for _ in range(epoch):
+        start = time.time()
+        result = model1.predict(img_path)
+        print('device: gpu, result:%s, time: %.4f' % ( str(result),time.time()-start) )
+    end_gpu = time.time()
     print('cpu avg time: %.4f' % ((end_cpu-start_cpu)/epoch))
-    # print('gpu avg time: %.4f' % (( end_gpu-start_gpu)/epoch))
+    print('gpu avg time: %.4f' % (( end_gpu-start_gpu)/epoch))
--- train.py
+++ train.py
@@ -14,7 +14,7 @@ if use_gpu:
     os.environ["CUDA_VISIBLE_DEVICES"] = gpu_id
     print('train with gpu:', gpu_id)
 
-num_epochs = 20
+num_epochs = 3
 batch_size = 64
 
 # train_data = torchvision.datasets.ImageFolder(root='/data/datasets/mnist/mnist_img/test',
@@ -22,11 +22,11 @@ batch_size = 64
 #                                                      [transforms.Resize(224), transforms.ToTensor()]))
 # train_loader = Data.DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
 
-train_data = MyDataset(txt='/data/datasets/mnist/mnist_img/mnist_test_label.txt',data_shape=(224,224),channel=3,
+train_data = MyDataset(txt='/data/datasets/mnist/mnist_img/mnist_train_label.txt',data_shape=(224,224),channel=3,
                        transform=transforms.ToTensor())
 train_loader = Data.DataLoader(dataset=train_data, batch_size=64, shuffle=True)
 
-model = torchvision.models.resnet18(num_classes=10)
+model = torchvision.models.resnet50(num_classes=10)
 if use_gpu:
     model.cuda()
 
@@ -62,4 +62,4 @@ for epoch in range(num_epochs):
     print('epoch [%d/%d], loss: %.4f, acc: %.4f, time:%0.4f' % (
         epoch + 1, num_epochs, train_loss / len(train_data), train_acc / len(train_loader.dataset),
         time.time() - start))
-torch.save(model, 'resnet18.pkl')
+torch.save(model, 'resnet50.pkl')
