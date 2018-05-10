47f243c1b8d8680205392e2f62721ef9a4c84857
zhoujun <zjwenmu@gmail.com>
2018-05-10 15:31:32 +0800

    add evaal acc

--- train.py
+++ train.py
@@ -11,6 +11,21 @@ print('training with:', device)
 num_epochs = 3
 batch_size = 64
 
+
+def evaluate_accuracy(test_loader, net):
+    net.eval()
+    n = 0
+    acc = 0
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+        out = net(images)
+        _, preds = torch.max(out.data, 1)
+        correct = preds.eq(labels.data).sum().item()
+        acc += correct
+        n += labels.size(0)
+    return acc / n
+
+
 # train_data = torchvision.datasets.ImageFolder(root='/data/datasets/mnist/train',
 #  transform=transforms.Compose(
 #  [transforms.Resize(227), transforms.ToTensor()]))
@@ -20,28 +35,34 @@ train_data = MyDataset(txt='/data/datasets/mnist/train.txt', data_shape=(227, 22
 train_loader = Data.DataLoader(
     dataset=train_data, batch_size=batch_size, shuffle=True, num_workers=3)
 
-model = torchvision.models.AlexNet(num_classes=10)
+testdata = MyDataset(txt='/data/datasets/mnist/test.txt', data_shape=(227, 227), channel=3,
+                     transform=transforms.ToTensor())
+test_loader = Data.DataLoader(
+    dataset=train_data, batch_size=batch_size, shuffle=True, num_workers=3)
+
+net = torchvision.models.AlexNet(num_classes=10)
 # 准备写tensorboard, 必须放在'.to(device)'之前，不然会报错
 writer = SummaryWriter()
 dummy_input = torch.autograd.Variable(torch.Tensor(1, 3, 227, 227))
-writer.add_graph(model=model, input_to_model=dummy_input)
+writer.add_graph(model=net, input_to_model=dummy_input)
 
-model = model.to(device)
+net = net.to(device)
 
 criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 5, gamma=0.1)
-model.train()
 
 for epoch in range(num_epochs):
+    net.train()
     scheduler.step()
     train_acc, train_loss = 0., 0.
     start = time.time()
+    cur_step = 0
     for i, (images, labels) in enumerate(train_loader):
         images, labels = images.to(device), labels.to(device)
         # Forward
         optimizer.zero_grad()
-        out = model(images)
+        out = net(images)
         # Backward
         loss = criterion(out, labels)
         loss.backward()
@@ -52,14 +73,15 @@ for epoch in range(num_epochs):
         acc = correct / labels.size(0)
         train_acc += correct
 
-        cur_step = epoch * (train_data.__len__()/batch_size)+i
+        cur_step = epoch * (train_data.__len__() / batch_size) + i
         writer.add_scalar(tag='Train/loss', scalar_value=loss.item(), global_step=cur_step)
         writer.add_scalar(tag='Train/acc', scalar_value=acc, global_step=cur_step)
         # if i % 1 == 0:
         # print('Iteration: {}. Loss: {}. Accuracy: {}'.format(cur_step, loss.item(), acc))
-    print('epoch [%d/%d], loss: %.4f, acc: %.4f, time:%0.4f, lr:%s' % (
-        epoch + 1, num_epochs, train_loss /
-        len(train_data), train_acc / len(train_loader.dataset),
+    eval_acc = evaluate_accuracy(test_loader, net)
+    writer.add_scalar(tag='EVAL/acc', scalar_value=eval_acc, global_step=cur_step)
+    print('epoch [%d/%d], train_loss: %.4f, train_acc: %.4f, eval_acc: %.4f, time:%0.4f, lr:%s' % (
+        epoch + 1, num_epochs, train_loss / len(train_data), train_acc / len(train_loader.dataset), eval_acc,
         time.time() - start, str(scheduler.get_lr()[0])))
 writer.close()
 # torch.save(model, 'AlexNet1.pkl')
